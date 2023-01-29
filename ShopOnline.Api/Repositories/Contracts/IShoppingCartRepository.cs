cea2653f90c711c4251958b33cecd1ea4e16af90
lukaszsereda <lukkser123@gmail.com>
2023-01-29 13:29:45 +0100

    Initial commit

---
+++ ShopOnline.Api/Repositories/Contracts/IShoppingCartRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShopOnline.Api.Entities;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Repositories.Contracts
+{
+    public interface IShoppingCartRepository
+    {
+        Task<CartItem> AddItem(CartItemToAddDto cartItemToAddDto);
+        Task<CartItem> UpdateQty(int id, CartItemQtyUpdateDto cartItemQtyUpdateQty);
+        Task<CartItem> DeleteItem(int id);
+        Task<CartItem> GetItem(int id);
+        Task<IEnumerable<CartItem>> GetItems(int userId);
+
+    }
+}
